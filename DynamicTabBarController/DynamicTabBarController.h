d8cd79c1b58a4f9d26eb350705a2d893250c5394
Nathan Tannar <nathantannar4@gmail.com>
2017-11-14 23:17:10 -0800

    Initial Commit

---
+++ DynamicTabBarController/DynamicTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  DynamicTabBarController.h
+//  DynamicTabBarController
+//
+//  Created by Nathan Tannar on 11/14/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicTabBarController.
+FOUNDATION_EXPORT double DynamicTabBarControllerVersionNumber;
+
+//! Project version string for DynamicTabBarController.
+FOUNDATION_EXPORT const unsigned char DynamicTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicTabBarController/PublicHeader.h>
+
+
